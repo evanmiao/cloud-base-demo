1e361ced2e7c69898247c98d18df4ad349558eec
Evan Miao <evan.c.miao@gmail.com>
2020-09-24 17:53:27 +0800

    init repo

---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  data() {
+    return {
+
+    }
+  },
+  onLoad(options) {
+
+	},
+  methods: {
+
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
